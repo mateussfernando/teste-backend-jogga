931181a4fb052b7be04af756ecf8ca902a2b936b
Mateus <mateusfss368@gmail.com>
2025-09-24 03:54:06 -0300

    fix: alterando porta de rota

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import leadRoutes from "./routes/leadRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
